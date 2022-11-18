815efaf8fff7b7551b0eaa39bd6ce14594c811e3
Dmitry Kozhevnikov <kdaufa@mail.ru>
2022-11-19 02:49:11 +0500

    app moved to OOP

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+// Интерфейс описывающий само приложение, и приложение
+import express, { Express } from "express";
+import { userRouter } from "./users/users";
+import { Server } from 'http';
+
+export class App {
+    app: Express;
+    server: Server;
+    port: number;
+
+    constructor() {
+        this.app = express();
+        this.port = 8000;
+    }
+
+
+    useRoutes() {
+        this.app.use('/users', userRouter);
+
+    }
+
+
+    // метод инициализации приложения
+    public async init() {
+        this.useRoutes();
+        this.server = this.app.listen(this.port);
+        console.log(`Сервер запущен на http://localhost:${this.port}`);
+
+        
+    }
+}
+
